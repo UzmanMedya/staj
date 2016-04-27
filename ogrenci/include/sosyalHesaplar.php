8646d8ced1e7d531e0db1a78185e596e182f97ae
Burak FIRAT <burakfiratz@gmail.com>
2016-04-27 15:22:16 +0300

    bootstrap deneme 5

--- ogrenci/include/sosyalHesaplar.php
+++ ogrenci/include/sosyalHesaplar.php
@@ -3,31 +3,31 @@
 	<div class="satir">
 	<div class="sol">Web Sitesi:</div>
 	<div class="sag">
-	<input type="text" name="websitesi" class="inputText">
+	<input type="text" name="websitesi" class="form-control" />
 	</div>
 	</div>
 	
 	<div class="satir">
 	<div class="sol">Facebook:</div>
 	<div class="sag">
-	<input type="text" name="facebook"  class="inputText">
+	<input type="text" name="facebook" class="form-control" />
 	</div>
 	</div>
 	
 	<div class="satir">
 	<div class="sol">Github:</div>
 	<div class="sag">
-	<input type="text" name="github"  class="inputText">
+	<input type="text" name="github" class="form-control" />
 	</div>
 	</div>
 	
 	<div class="satir">
 	<div class="sol">Gmail:</div>
 	<div class="sag">
-	<input type="email" name="gmail"  class="inputText">
+	<input type="email" name="gmail" class="form-control" />
 	</div>
 	</div>
 	
-<input type="button" value="KAYDET" name="kaydet" class="buttonSosyalHesapKaydet">
+<input type="button" value="KAYDET" name="kaydet" class="btn btn-default" />
 </form>
 </div>
