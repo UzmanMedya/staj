fafd46581c09766ea6eac1c2841855cb8332b2bb
burak95 <burakbuyurukcu214@gmail.com>
2016-04-27 15:55:48 +0300

    akademisyen profil gor

---
+++ akademisyen/include/akademisyenGor.php
@@ -0,0 +1,45 @@
+<html>
+<head>
+<link href="akademisyenGor.css" rel="stylesheet" type="text/css" />
+</head>
+<body>
+
+<div id="akademisyenGor">
+
+	<div id="fotoraf">
+		<img src="img/github.png"></img>
+	</div>
+	
+	<div id="adSoyad">
+		<h2><label>Burak Buyurukcu</label></h2>
+	</div>
+	
+	<div id="temizlemeDiv"></div>
+	
+	<div id="bilgiler">
+		<h3>
+			<ol>
+				<li>Temel Bilgiler</li>
+				<ul>
+					<li>Web Site : <a href="@"><label>www.burakbuyurukcu.com.tr</label></a></li>
+					<li>Telefon : <label>05462078928</label></li>
+				</ul>
+				<li>Eğitim Bilgileri</li>
+				<ul>
+					<li><label>KARADENİZ TEKNİK ÜNİVERSİTESİ</label></li>
+				</ul>
+				<li>Sosyal Ağlar : </li>
+				<ul>
+					<li><a href="@"><img src="img/facebook.png"></img></a>
+					<a href="@"><img src="img/twitter.png"></img></a>
+					<a href="@"><img src="img/gmail.png"></img></a></li>
+				</ul>
+				
+				
+			</ol>
+		</h3>
+		
+	</div>
+</div>
+</body>
+</html>
