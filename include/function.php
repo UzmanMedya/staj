a5ba0dcdc2c5f9ee7c8d3413bfd1353dff6caf94
Ömür Buruk <burukomur@gmail.com>
2016-04-27 14:24:58 +0300

    Giriş Yap Sayfa Goster

--- include/function.php
+++ include/function.php
@@ -41,37 +41,37 @@
 		$mail=temizle($mail);
 		$sifre=MD5($sifre);
 		global $conn;
-		$query ="select * from uyeol_login where mail='$mail' and parola='$sifre' and onay=1";
+		$query ="select * from tbl_kullanici where mail='$mail' and parola='$sifre' and onay=1";
 		
 		$sonuc =mysqli_query($conn,$query);
 		$sayi=@mysqli_num_rows($sonuc);
 		if($sayi>0){
 			$array=mysqli_fetch_array($sonuc);
-			$rol=$array["rolu"];
+			$rol=$array["rol"];
 			$mail=$array["mail"];
 			$id=$array["id"];
-			if($rol=="öğrenci"){
-				$query ="select * from  ogrenci where login_id='$id' ";
+			if($rol=="1"){
+				$query ="select * from  tbl_ogrenci where login_id='$id' ";
 				$sonuc =mysqli_query($conn,$query);
 				$array=mysqli_fetch_array($sonuc);
 			
-				$_SESSION["staj"]=new Session($array["login_id"],$array["adi"],$array["soyadi"],$array["mail"],"öğrenci");
+				$_SESSION["staj"]=new Session($array["user_id"],$array["adi"],$array["soyadi"],$array["mail"],"öğrenci");
 				printf( $_SESSION["staj"]);
 			}
-			if($rol=="akademisyen"){
-				$query ="select * from  akademisyen where login_id='$id' ";
+			if($rol=="2"){
+				$query ="select * from  tbl_akademisyen where user_id='$id' ";
 				$sonuc =mysqli_query($conn,$query);
 				$array=mysqli_fetch_array($sonuc);
 				
-				$_SESSION["staj"]=new Session($array["login_id"],$array["ad"],$array["soyad"],$rol,$mail);
+				$_SESSION["staj"]=new Session($array["user_id"],$array["ad"],$array["soyad"],$rol,$mail);
 				header("Location: akademisyen/index.php");
 			}
-			if($rol=="isyeri"){
-				$query ="select * from  isyeri where login_id='$id' ";
+			if($rol=="3"){
+				$query ="select * from  tbl_isyeri where user_id='$id' ";
 				$sonuc =mysqli_query($conn,$query);
 				$array=mysqli_fetch_array($sonuc);
 			
-				$_SESSION["staj"]=new Session($array["login_id"],$array["adi"],$array["soyadi"],$array["mail"],"isyeri");
+				$_SESSION["staj"]=new Session($array["user_id"],$array["adi"],$array["adi"],$array["aciklama"],"isyeri");
 					printf( $_SESSION["staj"]);
 			}
 		}
---
+++ isyeri/include/function.php
@@ -0,0 +1,36 @@
+<?php
+	
+	require_once("../../include/config.php");
+
+	function sayfa_getir()
+	{
+		$sayfa_adi=@$_GET["sayfa"];
+		if($sayfa_adi=="profil"){
+			include_once("profil.php");
+		}
+		else if($sayfa_adi=="hakkinda"){
+			include_once("hakkinda.php");
+		}
+		else if($sayfa_adi=="projeler"){
+			include_once("projeler.php");
+		}
+		else if($sayfa_adi=="sosyalhesaplar"){
+			include_once("sosyalhesaplar.php");
+		}
+		else
+		{
+		  include_once("profil.php");
+		}
+	}
+
+	function temizle($text)
+	{
+		$text =htmlspecialchars($text);
+		//...diðer temizleme iþlemleri
+		return $text;
+	}
+
+	
+	
+
+?>
