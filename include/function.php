f818383696519886517ede31afc424c5737953ca
KuzuRyuSen <halitak63@gmail.com>
2016-05-02 15:35:26 +0300

    düzeltme
    düzeltme

--- include/function.php
+++ include/function.php
@@ -103,7 +103,7 @@
 			
 			$query2 ="INSERT INTO tbl_isyeri('adi', 'il', 'ilce', 'adres', 'aciklama', 'user_id')
 			VALUES ('".$isyeri->getAd()."','".$isyeri->getIl()."','".$isyeri->getIlce()."','".$isyeri->getAdres()."','".$isyeri->getHizmet()."',,'".$id_gelen."')";
-			$sonuc2=mysqli_query($conn,$query2)
+			$sonuc2=mysqli_query($conn,$query2);
 			if($sonuc2){
 				echo "eklendi";
 			}
