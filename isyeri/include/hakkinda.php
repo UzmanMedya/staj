4c4a39696010b63744fdb52e802539eeeebc584c
KuzuRyuSen <halitak63@gmail.com>
2016-05-05 13:22:30 +0300

    hakkında ekle(isyeri)
    hakkında ekle

--- isyeri/include/hakkinda.php
+++ isyeri/include/hakkinda.php
@@ -1,13 +1,28 @@
+﻿<?php
+	if(@$_POST["ekle"]){
+		require_once("hakkinda_class.php");
+		
+		$hakkinda=new Hakkinda();
+		
+		$hakkinda->setBaslik(@$_POST["baslik"]);
+		$hakkinda->setIcerik(@$_POST["icerik"]);
+		
+		hakkinda_ekle($hakkinda);
+	}
+?>
+
 <link rel="stylesheet" type="text/css" href="../css/hakkinda.css">
+<form id="hakkinda" action="" method="post">
 <div id="isyeri_hakkinda">
 <label id="isyeri_hakkinda_baslik">
 Önsöz :
 </label>
-<input type="text" id="isyeri_hakkinda_onsoz" value=""> <br/>
+<input type="text" id="isyeri_hakkinda_onsoz" value="" name="baslik"> <br/>
 <label id="isyeri_hakkinda_icerik">
 İçerik :
 </label>
-<textarea id="isyeri_hakkinda_textarea" ></textarea><br>
+<textarea id="isyeri_hakkinda_textarea" name="icerik"></textarea><br>
 
-<input id="isyeri_hakkinda_duzenle" type="submit" value="Düzenle" >
+<input id="isyeri_hakkinda_duzenle" type="submit" value="Ekle" name="ekle">
 </div>
+</form>
